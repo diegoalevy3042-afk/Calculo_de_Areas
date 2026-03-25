76fe6ed52ed8f9a9ea728af3eab20d09c380984f
Tony Alexander <tonyperez06324@gmail.com>
2026-03-25 15:02:07 -0600

    Esta es mi chamba para la plebada de los MLS Rifa2 100%

--- Area_de_figeuras/src/formulas/circulo.java
+++ Area_de_figeuras/src/formulas/circulo.java
@@ -4,15 +4,20 @@ package formulas;
  *
  * @author Pc29
  */
-public class circulo {
-        private double radio;
-        
-        public circulo(double radio){
-        this.radio = radio;
-        }
-        
-        public double calcularArea(){
-        return (radio * radio) * 3.1416;
-        }
 
+public class circulo {
+    private double radio;
+
+    public circulo(double radio) {
+        this.radio = radio;
+    }
+    
+//Utilizamos el metodo Math especializado en calculos matematicos
+//Biene por default en java.
+    
+    public double calcularArea() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+    
 }
+
